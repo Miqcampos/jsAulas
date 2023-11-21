0774b57473a3d6d76d02dcc11d257426915c89fb
Miqcampos <miqueiascampos344@gmail.com>
2023-11-21 17:17:11 -0300

    aula001
    atualização

---
+++ aula001/materialDeAula/js/script02.js
@@ -0,0 +1,10 @@
+alert("Calculadora de soma!");
+var numero1 = Number(prompt("Digite o seu primeiro número!"));
+var numero2 = Number(prompt("Digite o seu segundo número!"));
+
+// primeira maneira
+// var soma = parseFloat(numero1) + parseFloat(numero2);
+
+var soma = numero1 + numero2;
+
+alert(soma);
