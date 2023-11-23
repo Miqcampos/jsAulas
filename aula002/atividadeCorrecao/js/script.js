58aee404718886d78124e083f22d1202aa5f0171
Miqcampos <miqueiascampos344@gmail.com>
2023-11-23 14:53:43 -0300

    aula002
    condicional

---
+++ aula002/atividadeCorrecao/js/script.js
@@ -0,0 +1,34 @@
+var idade = Number(prompt("Digite a sua idade:"))
+
+// if
+
+// if(idade <= 0){
+//     alert("Idade inválida!");
+//     location.reload();
+// }
+
+// if(idade > 0 && idade < 15){
+//     alert("Faixa etária: criança!");
+//     location.reload();
+// }
+
+// if(idade >= 15 && idade < 30){
+//     alert("Faixa etária: Faixa etária: jovem!");
+//     location.reload();
+// }
+
+// if(idade >= 30 && idade < 60){
+//     alert("Faixa etária: adulto!");
+//     location.reload();
+// }
+
+// if(idade >= 60){
+//     alert("Faixa etária: idoso!");
+//     location.reload();
+// }
+
+// if else
+
+///////////////////////////////////////////////////////////
+
+// else if
